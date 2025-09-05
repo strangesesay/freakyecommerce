3f94215773d78811f8d0479e7c772137696dbb54
strangesesay <126430875+strangesesay@users.noreply.github.com>
2025-09-05 05:27:34 +0000

    updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,21 @@
+
 import { Component} from '@angular/core';
 import {Header} from './components/header/header';
 import {Productcard} from './components/productcard/productcard';
-
+import { RouterModule } from '@angular/router';
+import { Carts } from './components/carts/carts';
 
 @Component({
   selector: 'app-root',
   imports: [
-    Header,
+    Header, 
     Productcard
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
+
 export class App {
 
 }
